411ee12f4263bc9329a86fc4eb651742624707ac
syahrul250700 <58725451+syahrul250700@users.noreply.github.com>
2021-03-31 15:07:59 +0700

    Update Portfolio.php

--- application/controllers/Portfolio.php
+++ application/controllers/Portfolio.php
@@ -6,7 +6,7 @@ class Portfolio extends CI_Controller
 	{
 		$data['judul'] = 'Halaman Portfolio';
 
-		$data['Portfolio'] = $this->db->get('portfolio')->result_array();
+		
 		
 		$this->load->view('templates/header', $data);
 		$this->load->view('portfolio/index', $data);
