5f2466b3019e0c9500c3cbdfcfe3a61a5b3617cf
syahrul250700 <58725451+syahrul250700@users.noreply.github.com>
2021-03-31 14:27:32 +0700

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'file', 'form' , 'security', 'sah');
+$autoload['helper'] = array('url', 'file', 'form' , 'security');
 
 /*
 | -------------------------------------------------------------------
