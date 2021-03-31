5712c93f184da25b91b8b1d51dfc9277b4848e1f
syahrul250700 <58725451+syahrul250700@users.noreply.github.com>
2021-03-31 14:20:15 +0700

    Update Contact.php

--- application/controllers/Contact.php
+++ application/controllers/Contact.php
@@ -13,14 +13,14 @@ class Contact extends CI_Controller
 		$this->load->view('contact/index', $data);
 		$this->load->view('templates/footer');
 	}
- 	function send(){
-	    $nama=htmlspecialchars($this->input->post('nama',TRUE),ENT_QUOTES);
-		$email=htmlspecialchars($this->input->post('email',TRUE),ENT_QUOTES);
-		$no_telepon=htmlspecialchars($this->input->post('no_telepon',TRUE),ENT_QUOTES);
-		$pesan=htmlspecialchars(trim($this->input->post('pesan',TRUE)),ENT_QUOTES);
-		$this->load->model('Contact_model');
-		$this->Contact_model->sendContact($nama,$email,$no_telepon,$pesan);
-		echo $this->session->set_flashdata('msg',"<div class='alert alert-info'>Terima kasih telah menghubungi kami.</div>");
-		redirect('contact');
-}
-}
+//  	function send(){
+// 	    $nama=htmlspecialchars($this->input->post('nama',TRUE),ENT_QUOTES);
+// 		$email=htmlspecialchars($this->input->post('email',TRUE),ENT_QUOTES);
+// 		$no_telepon=htmlspecialchars($this->input->post('no_telepon',TRUE),ENT_QUOTES);
+// 		$pesan=htmlspecialchars(trim($this->input->post('pesan',TRUE)),ENT_QUOTES);
+// 		$this->load->model('Contact_model');
+// 		$this->Contact_model->sendContact($nama,$email,$no_telepon,$pesan);
+// 		echo $this->session->set_flashdata('msg',"<div class='alert alert-info'>Terima kasih telah menghubungi kami.</div>");
+// 		redirect('contact');
+// }
+ }
