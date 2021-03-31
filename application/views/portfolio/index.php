8141b7ab0610ab59225ae9c670956ecb00b8bf9f
syahrul250700 <58725451+syahrul250700@users.noreply.github.com>
2021-03-31 16:19:39 +0700

    Update index.php

--- application/views/portfolio/index.php
+++ application/views/portfolio/index.php
@@ -11,7 +11,7 @@
         
       	<div class="col-sm-4">
       		<a href="">
-      			<img src="<?= base_url(); ?>/assets/img/kyoto.jpg ?>" height="400"class="card-img-top" alt="...">
+      			<img src="<?= base_url(); ?>/assets/img/kyoto.jpg" height="400"class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title text-center dark">Paket Tour Kyoto Jepang</h5>
               <p class="card-text"><a href="" class="badge badge-primary">detail</a></small></p>
@@ -20,7 +20,7 @@
       	</div>
        <div class="col-sm-4">
         <a href="">
-        <img src="<?= base_url(); ?>/assets/img/maldives.jpg ?>" height="400"class="card-img-top" alt="...">
+        <img src="<?= base_url(); ?>/assets/img/maldives.jpg" height="400"class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title text-center">Paket Tour Maldives</h5>
           <p class="card-text"><a href="" class="badge badge-primary">detail</a></small></p>
@@ -29,7 +29,7 @@
     </div>
     <div class="col-sm-4">
         <a href="">
-        <img src="<?= base_url(); ?>/assets/img/bali.jpg ?>" height="400"class="card-img-top" alt="...">
+        <img src="<?= base_url(); ?>/assets/img/bali.jpg" height="400"class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title text-center">Paket Tour Bali Indonesia</h5>
           <p class="card-text"><a href="" class="badge badge-primary">detail</a></small></p>
@@ -38,7 +38,7 @@
     </div>
     <div class="col-sm-4">
         <a href="">
-        <img src="<?= base_url(); ?>/assets/img/paris.jpg ?>" height="400"class="card-img-top" alt="...">
+        <img src="<?= base_url(); ?>/assets/img/paris.jpg" height="400"class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title text-center">Paket Tour Paris Perancis</h5>
           <p class="card-text"><a href="" class="badge badge-primary">detail</a></small></p>
